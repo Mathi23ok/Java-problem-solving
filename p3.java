8c2503e97e8609dff596ad2bd68308417b2b2b3b
Sanjana Joseph <164984683+Mathi23ok@users.noreply.github.com>
2025-04-23 12:09:10 +0530

    Create p3.java

---
+++ p3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+      for(int i =1;i<=a*3;i++){
+        System.out.print(i+ " ");
+        i+=2;
+        
+    
+      }
+
+  }
+}
+//Write a program to print the series 1,4,7,10 upto n term
