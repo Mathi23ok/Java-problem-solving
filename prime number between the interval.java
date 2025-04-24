d0c435fde233a48eb8b4c9405c78429df60f168f
Sanjana Joseph <164984683+Mathi23ok@users.noreply.github.com>
2025-04-24 08:45:19 +0530

    Update prime number between the interval.java

--- prime number between the interval.java
+++ prime number between the interval.java
@@ -19,3 +19,26 @@ public class Main {
         }
   }
 }
+//optimistic approach with flag
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+        int b = sc.nextInt();
+        boolean c = true;
+        for(int i = a;i<=b;i++){
+          for(int j =2;j<=i/2;j++){
+            if(i%j==0){
+              c = false;
+              break;
+            }
+          }
+          if(c){
+            System.out.print(i+" ");
+          }
+          c = true; //reassign the value
+        }
+  }
+}
