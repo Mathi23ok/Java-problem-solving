f120034908a3d40963afaad006b6c367baf99313
Sanjana Joseph <164984683+Mathi23ok@users.noreply.github.com>
2025-04-24 14:36:23 +0530

    Update to check for a Armstrong number.java

--- to check for a Armstrong number.java
+++ to check for a Armstrong number.java
@@ -29,3 +29,27 @@ public class Main {
      }
     }
 }
+//Another solution
+import java.util.*;
+public class Main {
+public static void main(String args[]){
+ Scanner obj = new Scanner(System.in);
+ int a = obj.nextInt();
+ int temp = a , Armstrong = 0 , count = 0 , power=1; 
+ while(temp > 0 ){ 
+   count++; 
+   temp/=10;
+ } 
+ temp = a ; 
+ while( temp > 0) {  
+   for(int i=1;i<=count;i++) {  
+     power *= (temp%10) ;  
+   }
+   Armstrong += power ; 
+   power = 1 ; 
+   //Armstrong += Math.pow(temp%10,count);
+   temp/=10 ;  
+ }
+ System.out.print((a==Armstrong)?"YES":"NO");
+ }
+}
