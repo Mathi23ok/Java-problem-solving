1075c687c9d500a44d840d1fddce637c9fa289ef
Sanjana Joseph <164984683+Mathi23ok@users.noreply.github.com>
2025-04-24 08:32:38 +0530

    Create prime number between the interval.java

---
+++ prime number between the interval.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c =0;
+        for(int i = a;i<=b;i++){
+          for(int j =1;j<=i;j++){
+            if(i%j==0){
+              c+=1;
+            }
+          }
+          if(c==2){
+            System.out.print(i+" ");
+          }
+          c = 0;
+        }
+  }
+}
